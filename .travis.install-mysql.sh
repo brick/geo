58e4c914de549ff279dcb97e1a723f746b1c1b3d
Benjamin Morel <benjamin.morel@gmail.com>
2015-03-15 00:13:57 +0100

    Changed MySQL 5.6 installation method on Travis
    This should fix issues with connections from HHVM.

--- .travis.install-mysql.sh
+++ .travis.install-mysql.sh
@@ -1,8 +1,13 @@
-sudo apt-get remove --purge "^mysql.*"
+sudo apt-get remove mysql-server-5.5 mysql-server-core-5.5
 sudo apt-get autoremove
-sudo apt-get autoclean
-sudo apt-add-repository ppa:ondrej/mysql-5.6 -y
-sudo apt-get update
-sudo apt-get install mysql-server-5.6 mysql-client-5.6
-mysql -uroot -e "SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION"
+sudo apt-get install libaio1
+wget http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.23-debian6.0-x86_64.deb
+sudo dpkg -i mysql-5.6.23-debian6.0-x86_64.deb
+sudo cp /opt/mysql/server-5.6/support-files/mysql.server /etc/init.d/mysql.server
+sudo ln -sf /opt/mysql/server-5.6/bin/* /usr/bin/
+sudo sed -i'' 's/table_cache/table_open_cache/' /etc/mysql/my.cnf
+sudo sed -i'' 's/log_slow_queries/slow_query_log/' /etc/mysql/my.cnf
+sudo sed -i'' 's/basedir[^=]\+=.*$/basedir = \/opt\/mysql\/server-5.6/' /etc/mysql/my.cnf
+sudo /etc/init.d/mysql.server start
+mysql -u root -e "SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION"
 mysql --version
