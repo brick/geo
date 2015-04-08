39d31094f77b71edcfd05064ed2973a848b76883
Benjamin Morel <benjamin.morel@gmail.com>
2015-04-08 14:20:37 +0200

    Added tests for Polygon class

--- tests/PolygonTest.php
+++ tests/PolygonTest.php
@@ -2,6 +2,7 @@
 
 namespace Brick\Geo\Tests;
 
+use Brick\Geo\Exception\GeometryException;
 use Brick\Geo\LineString;
 use Brick\Geo\Polygon;
 
@@ -25,9 +26,6 @@ class PolygonTest extends AbstractTestCase
         $this->assertSame($is3D, $polygon->is3D());
         $this->assertSame($isMeasured, $polygon->isMeasured());
         $this->assertSame($srid, $polygon->SRID());
-
-        $expectedExteriorRing = LineString::create([], $is3D, $isMeasured, $srid);
-        $this->assertWktEquals($polygon->exteriorRing(), $expectedExteriorRing->asText(), $srid);
     }
 
     /**
@@ -44,6 +42,164 @@ class PolygonTest extends AbstractTestCase
             [true ,false, 4326],
             [false, true, 4326],
             [true, true, 4326]
+        ];
+    }
+
+    /**
+     * @dataProvider providerExteriorRing
+     *
+     * @param string $polygon      The WKT of the Polygon to test.
+     * @param string $exteriorRing The WKT of the expected exterior ring.
+     */
+    public function testExteriorRing($polygon, $exteriorRing)
+    {
+        foreach ([0, 1] as $srid) {
+            $ring = Polygon::fromText($polygon, $srid)->exteriorRing();
+            $this->assertWktEquals($ring, $exteriorRing, $srid);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function providerExteriorRing()
+    {
+        return [
+            ['POLYGON ((1 2, 1 3, 2 2, 1 2))', 'LINESTRING (1 2, 1 3, 2 2, 1 2)'],
+            ['POLYGON ((0 0, 0 9, 9 9, 0 0), (1 1, 1 8, 8 8, 1 1))', 'LINESTRING (0 0, 0 9, 9 9, 0 0)'],
+            ['POLYGON Z ((1 2 3, 4 5 6, 7 8 9, 1 2 3))', 'LINESTRING Z (1 2 3, 4 5 6, 7 8 9, 1 2 3)'],
+            ['POLYGON M ((1 2 3, 4 5 6, 7 8 9, 1 2 3))', 'LINESTRING M (1 2 3, 4 5 6, 7 8 9, 1 2 3)'],
+            ['POLYGON ZM ((1 2 0 1, 1 3 0 2, 2 2 0 3, 1 2 0 1))', 'LINESTRING ZM (1 2 0 1, 1 3 0 2, 2 2 0 3, 1 2 0 1)'],
+        ];
+    }
+
+    /**
+     * @dataProvider providerExteriorRingOfEmptyPolygon
+     * @expectedException \Brick\Geo\Exception\GeometryException
+     *
+     * @param string $polygon The WKT of the polygon to test.
+     */
+    public function testExteriorRingOfEmptyPolygon($polygon)
+    {
+        Polygon::fromText($polygon)->exteriorRing();
+    }
+
+    /**
+     * @return array
+     */
+    public function providerExteriorRingOfEmptyPolygon()
+    {
+        return [
+            ['POLYGON EMPTY'],
+            ['POLYGON Z EMPTY'],
+            ['POLYGON M EMPTY'],
+            ['POLYGON ZM EMPTY'],
+        ];
+    }
+
+    /**
+     * @dataProvider providerNumInteriorRings
+     *
+     * @param string  $polygon          The WKT of the Polygon to test.
+     * @param integer $numInteriorRings The expected number of interior rings.
+     */
+    public function testNumInteriorRings($polygon, $numInteriorRings)
+    {
+        $polygon = Polygon::fromText($polygon);
+        $this->assertSame($numInteriorRings, $polygon->numInteriorRings());
+    }
+
+    /**
+     * @return array
+     */
+    public function providerNumInteriorRings()
+    {
+        return [
+            ['POLYGON EMPTY', 0],
+            ['POLYGON Z EMPTY', 0],
+            ['POLYGON M EMPTY', 0],
+            ['POLYGON ZM EMPTY', 0],
+            ['POLYGON ((0 0, 0 1, 1 1, 0 0))', 0],
+            ['POLYGON ((0 0, 0 9, 9 9, 0 0), (1 1, 1 8, 8 8, 1 1))', 1],
+            ['POLYGON ((0 0, 0 9, 9 9, 0 0), (1 2, 1 4, 2 4, 1 2), (1 5, 2 6, 2 5, 1 5))', 2],
+            ['POLYGON Z ((0 0 0, 0 1 0, 1 1 0, 0 0 0))', 0],
+            ['POLYGON Z ((0 0 0, 0 9 0, 9 9 0, 0 0 0), (1 1 0, 1 8 0, 8 8 0, 1 1 0))', 1],
+        ];
+    }
+
+    /**
+     * @dataProvider providerInteriorRingN
+     *
+     * @param string $polygon       The WKT of the Polygon to test.
+     * @param array  $interiorRings The expected interior rings.
+     *                              Keys are ring numbers, values are WKT of the expected rings.
+     *                              For invalid ring numbers, replacing the WKT with NULL will expect an exception.
+     */
+    public function testInteriorRingN($polygon, array $interiorRings)
+    {
+        foreach ($interiorRings as $n => $interiorRingN) {
+            if ($interiorRingN === null) {
+                $this->setExpectedException(GeometryException::class);
+            }
+
+            foreach ([0, 1] as $srid) {
+                $ring = Polygon::fromText($polygon, $srid)->interiorRingN($n);
+                $this->assertWktEquals($ring, $interiorRingN, $srid);
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function providerInteriorRingN()
+    {
+        return [
+            ['POLYGON EMPTY', [
+                0 => null,
+                1 => null,
+            ]],
+            ['POLYGON Z EMPTY', [
+                0 => null,
+                1 => null,
+            ]],
+            ['POLYGON M EMPTY', [
+                0 => null,
+                1 => null,
+            ]],
+            ['POLYGON ZM EMPTY', [
+                0 => null,
+                1 => null,
+            ]],
+            ['POLYGON ((0 0, 0 1, 1 1, 0 0))', [
+                0 => null,
+                1 => null,
+            ]],
+            ['POLYGON ((0 0, 0 9, 9 9, 0 0), (1 1, 1 8, 8 8, 1 1))', [
+                0 => null,
+                1 => 'LINESTRING (1 1, 1 8, 8 8, 1 1)',
+                2 => null,
+            ]],
+            ['POLYGON ((0 0, 0 9, 9 9, 0 0), (1 1, 1 8, 8 8, 1 1))', [
+                0 => null,
+                1 => 'LINESTRING (1 1, 1 8, 8 8, 1 1)',
+                2 => null,
+            ]],
+            ['POLYGON ((0 0, 0 9, 9 9, 0 0), (1 2, 1 4, 2 4, 1 2), (1 5, 2 6, 2 5, 1 5))', [
+                0 => null,
+                1 => 'LINESTRING (1 2, 1 4, 2 4, 1 2)',
+                2 => 'LINESTRING (1 5, 2 6, 2 5, 1 5)',
+                3 => null,
+            ]],
+            ['POLYGON Z ((0 0 0, 0 1 0, 1 1 0, 0 0 0))', [
+                0 => null,
+                1 => null,
+            ]],
+            ['POLYGON Z ((0 0 0, 0 9 0, 9 9 0, 0 0 0), (1 1 0, 1 8 0, 8 8 0, 1 1 0))', [
+                0 => null,
+                1 => 'LINESTRING Z (1 1 0, 1 8 0, 8 8 0, 1 1 0)',
+                2 => null,
+            ]],
         ];
     }
 }
