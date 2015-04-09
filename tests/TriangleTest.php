c9171888c7c9ee0771825cf0937a85fd7705eae7
Benjamin Morel <benjamin.morel@gmail.com>
2015-04-10 00:08:08 +0200

    Added tests for Triangle

---
+++ tests/TriangleTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Brick\Geo\Tests;
+
+use Brick\Geo\LineString;
+use Brick\Geo\Triangle;
+
+/**
+ * Unit tests for class Triangle.
+ */
+class TriangleTest extends AbstractTestCase
+{
+    public function testCreate()
+    {
+        $ring = LineString::fromText('LINESTRING (1 1, 1 2, 2 2, 1 1)');
+        $triangle = Triangle::create([$ring], false, false, 0);
+        $this->assertWktEquals($triangle, 'TRIANGLE ((1 1, 1 2, 2 2, 1 1))');
+    }
+
+    /**
+     * @expectedException \Brick\Geo\Exception\GeometryException
+     */
+    public function testCreateWithInvalidNumberOfPoints()
+    {
+        $ring = LineString::fromText('LINESTRING (1 1, 1 2, 2 2, 2 1, 1 1)');
+
+        Triangle::create([$ring], false, false, 0);
+    }
+
+    /**
+     * @expectedException \Brick\Geo\Exception\GeometryException
+     */
+    public function testCreateWithInteriorRings()
+    {
+        $exteriorRing = LineString::fromText('LINESTRING (0 0, 0 3, 3 3, 0 0)');
+        $interiorRing = LineString::fromText('LINESTRING (1 1, 1 2, 2 2, 1 1)');
+
+        Triangle::create([$exteriorRing, $interiorRing], false, false, 0);
+    }
+}
