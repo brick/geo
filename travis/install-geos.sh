9a9278d81944ba84067b14df421d6742e1ec2937
Benjamin Morel <benjamin.morel@gmail.com>
2016-12-10 02:32:47 +0100

    Fix Travis CI GEOS tests

--- travis/install-geos.sh
+++ travis/install-geos.sh
@@ -1,10 +1,23 @@
+sudo apt-get update
+sudo apt-get remove 'libgeos.*'
+sudo apt-get autoremove
+
 wget https://github.com/libgeos/libgeos/archive/$VERSION.tar.gz
 tar zxf $VERSION.tar.gz
 cd libgeos-$VERSION
 ./autogen.sh
-./configure --enable-php
+./configure --prefix=/usr
 make
-mv php/.libs/geos.so $(php-config --extension-dir)
+sudo make install
+cd ..
+
+wget https://git.osgeo.org/gogs/geos/php-geos/archive/1.0.0rc1.tar.gz
+tar zxf 1.0.0rc1.tar.gz
+cd php-geos
+./autogen.sh
+./configure
+make
+mv modules/geos.so $(php-config --extension-dir)
 cd ..
 
 echo "extension=geos.so" > geos.ini
