4c47e79f85c52849074b34b6b3fdb168df501798
Benjamin Morel <benjamin.morel@gmail.com>
2016-12-11 00:52:19 +0100

    Update SpatiaLite and GEOS versions in Travis CI

--- travis/install-spatialite.sh
+++ travis/install-spatialite.sh
@@ -1,21 +1,19 @@
 sudo apt-get update
 sudo apt-get install libproj-dev libfreexl-dev libxml2-dev
 
-wget http://download.osgeo.org/geos/geos-3.4.2.tar.bz2
-tar jxf geos-3.4.2.tar.bz2
-cd geos-3.4.2
+wget http://download.osgeo.org/geos/geos-3.6.0.tar.bz2
+tar jxf geos-3.6.0.tar.bz2
+cd geos-3.6.0
 ./configure
 make
 sudo make install
 
-wget http://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.0.0.tar.gz
-tar zxf libspatialite-4.0.0.tar.gz
-cd libspatialite-4.0.0
+wget http://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-4.2.0.tar.gz
+tar zxf libspatialite-4.2.0.tar.gz
+cd libspatialite-4.2.0
 ./configure
 make
 sudo make install
-
-ls -al /usr/local/lib/*spatialite*
 
 if [[ $TRAVIS_PHP_VERSION != hhvm ]]; then
     printf "[sqlite3]\nsqlite3.extension_dir = /usr/local/lib" > sqlite.ini
