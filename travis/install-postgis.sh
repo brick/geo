e234c1d81da72cde3ebfebeea4f3606308a1b05e
Benjamin Morel <benjamin.morel@gmail.com>
2020-12-07 17:09:14 +0100

    Fix Travis CI

--- travis/install-postgis.sh
+++ travis/install-postgis.sh
@@ -1,7 +1,7 @@
 set -e
 
 sudo apt update
-sudo apt install postgresql-10-postgis-2.5
+sudo apt install postgresql-10-postgis-2.4
 sudo service postgresql start 10
 
 psql -d postgres -c "CREATE EXTENSION postgis;"
--- travis/install-spatialite.sh
+++ travis/install-spatialite.sh
@@ -1,2 +1,2 @@
 apt install libsqlite3-mod-spatialite
-echo "sqlite3.extension_dir = /usr/lib/x86_64-linux-gnu" >> /home/travis/.phpenv/versions/8.0.0/etc/php.ini
+echo "sqlite3.extension_dir = /usr/lib/x86_64-linux-gnu" >> "/home/travis/.phpenv/versions/$PHP_VERSION/etc/php.ini"
