78d73a14917e196a6012a75213042514ce26af2d
Benjamin Morel <benjamin.morel@gmail.com>
2020-12-07 01:29:15 +0100

    Fix PGSQL on Travis, again

--- travis/install-postgis.sh
+++ travis/install-postgis.sh
@@ -3,4 +3,8 @@ set -e
 sudo apt-get update
 sudo apt-get autoremove postgis*
 sudo apt-get autoremove postgresql*
-sudo apt-get install postgresql-9.6-postgis-2.5
+
+if [[ "$TRAVIS_DIST" = "xenial" ]]
+then sudo apt-get install postgresql-9.6-postgis-2.5
+else sudo apt-get install postgresql-9.3-postgis-2.1
+fi
