4d15f3af73fbe47c638dbf838d019889a2280ae5
Benjamin Morel <benjamin.morel@gmail.com>
2020-12-10 10:44:35 +0100

    Fix PostgreSQL tests

--- travis/install-postgis.sh
+++ travis/install-postgis.sh
@@ -1,9 +1,7 @@
 set -e
 
 sudo apt update
-sudo apt autoremove postgis*
-sudo apt autoremove postgresql*
-sudo apt install postgresql-13-postgis-3
-sudo service postgresql start 13
+sudo apt install postgresql-11-postgis-2.5
+sudo service postgresql start 11
 
 psql -d postgres -c "CREATE EXTENSION postgis;"
