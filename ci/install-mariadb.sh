1e75f1d53558114b2ec2e129af69dcb30fdc235d
Benjamin Morel <benjamin.morel@gmail.com>
2020-12-14 11:19:24 +0100

    Move CI to GitHub Actions

--- ci/install-mariadb.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt install mariadb-server-10.1 mariadb-client-10.1
-
-echo "
-UPDATE mysql.user SET plugin = '' where User = 'root';
-FLUSH PRIVILEGES;
-" | sudo mysql
--- ci/install-spatialite.sh
+++
@@ -1,3 +0,0 @@
-apt update
-apt install libsqlite3-mod-spatialite
-echo "sqlite3.extension_dir = /usr/lib/x86_64-linux-gnu" >> "/home/travis/.phpenv/versions/$PHP_VERSION/etc/php.ini"
