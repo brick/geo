d9574e7882385d92a17151514aa04c7701527291
Benjamin Morel <benjamin.morel@gmail.com>
2025-02-16 11:49:24 +0100

    Add tests for WKBReader::readAsProxy()

--- tests/IO/WkbReaderTest.php
+++ tests/IO/WkbReaderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Brick\Geo\Tests\IO;
 
+use Brick\Geo\Geometry;
 use Brick\Geo\Io\WkbReader;
 use PHPUnit\Framework\Attributes\DataProvider;
 
@@ -24,6 +25,25 @@ class WkbReaderTest extends WkbAbstractTestCase
 
         self::assertSame($wkt, $geometry->asText());
         self::assertSame(4326, $geometry->srid());
+    }
+
+    /**
+     * @param string $wkb The WKB to read, hex-encoded.
+     * @param string $wkt The expected WKT output.
+     */
+    #[DataProvider('providerRead')]
+    public function testReadAsProxy(string $wkb, string $wkt) : void
+    {
+        $reader = new WKBReader();
+        $geometry = $reader->readAsProxy(hex2bin($wkb), 4326);
+
+        $reflectionClass = new \ReflectionClass(Geometry::class);
+        self::assertTrue($reflectionClass->isUninitializedLazyObject($geometry));
+
+        self::assertSame($wkt, $geometry->asText());
+        self::assertSame(4326, $geometry->SRID());
+
+        self::assertFalse($reflectionClass->isUninitializedLazyObject($geometry));
     }
 
     public static function providerRead() : \Generator
