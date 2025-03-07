b0e8807d8851e2c3fe6e0dd4722f46b2f17f0a19
Benjamin Morel <benjamin.morel@gmail.com>
2025-03-07 08:24:27 +0100

    WKBGeometryHeader: readonly + promoted properties

--- src/IO/WKBGeometryHeader.php
+++ src/IO/WKBGeometryHeader.php
@@ -4,21 +4,13 @@ declare(strict_types=1);
 
 namespace Brick\Geo\IO;
 
-/**
- * @psalm-immutable
- */
 final class WKBGeometryHeader
 {
-    public int $geometryType;
-    public bool $hasZ;
-    public bool $hasM;
-    public ?int $srid;
-
-    public function __construct(int $geometryType, bool $hasZ, bool $hasM, ?int $srid = null)
-    {
-        $this->geometryType = $geometryType;
-        $this->hasZ         = $hasZ;
-        $this->hasM         = $hasM;
-        $this->srid         = $srid;
+    public function __construct(
+        public readonly int $geometryType,
+        public readonly bool $hasZ,
+        public readonly bool $hasM,
+        public readonly ?int $srid = null,
+    ) {
     }
 }
