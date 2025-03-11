f74ec660373a09a488d11787a220727e2c5a0422
Benjamin Morel <benjamin.morel@gmail.com>
2025-03-11 16:16:42 +0100

    Always require mysql/pgsql env vars in tests

--- phpunit-bootstrap.php
+++ phpunit-bootstrap.php
@@ -4,28 +4,54 @@ use Brick\Geo\Engine\PDOEngine;
 use Brick\Geo\Engine\SQLite3Engine;
 use Brick\Geo\Engine\GEOSEngine;
 
+function getOptionalEnv(string $name): ?string
+{
+    $value = getenv($name);
+
+    return $value === false ? null : $value;
+}
+
+function getOptionalEnvOrDefault(string $name, string $default): string
+{
+    $value = getenv($name);
+
+    return $value === false ? $default : $value;
+}
+
+function getRequiredEnv(string $name): string
+{
+    $value = getenv($name);
+
+    if ($value === false) {
+        echo 'Missing environment variable: ', $name, PHP_EOL;
+        exit(1);
+    }
+
+    return $value;
+}
+
 (function() {
     require 'vendor/autoload.php';
 
-    $engine = getenv('ENGINE');
+    $engine = getOptionalEnv('ENGINE');
 
-    if ($engine === false) {
-        echo 'WARNING: running tests without a geometry engine.' . PHP_EOL;
-        echo 'All tests requiring a geometry engine will be skipped.' . PHP_EOL;
-        echo 'To run tests with a geometry engine, use: ENGINE={engine} vendor/bin/phpunit' . PHP_EOL;
-        echo 'Available engines: PDO_MYSQL, PDO_PGSQL, SQLite3, GEOS' . PHP_EOL;
+    if ($engine === null) {
+        echo 'WARNING: running tests without a geometry engine.', PHP_EOL;
+        echo 'All tests requiring a geometry engine will be skipped.', PHP_EOL;
+        echo 'To run tests with a geometry engine, use: ENGINE={engine} vendor/bin/phpunit', PHP_EOL;
+        echo 'Available engines: PDO_MYSQL, PDO_PGSQL, SQLite3, GEOS', PHP_EOL;
     } else {
         switch ($engine) {
             case 'PDO_MYSQL':
-                $emulatePrepares = getenv('EMULATE_PREPARES') === 'ON';
+                $emulatePrepares = getOptionalEnv('EMULATE_PREPARES') === 'ON';
 
-                echo 'Using PDOEngine for MySQL' . PHP_EOL;
-                echo 'with emulated prepares ' . ($emulatePrepares ? 'ON' : 'OFF') . PHP_EOL;
+                echo 'Using PDOEngine for MySQL', PHP_EOL;
+                echo 'with emulated prepares ', ($emulatePrepares ? 'ON' : 'OFF'), PHP_EOL;
 
-                $host = $_ENV['MYSQL_HOST'] ?? '127.0.0.1';
-                $port = $_ENV['MYSQL_PORT'] ?? '3306';
-                $username = $_ENV['MYSQL_USER'] ?? 'root';
-                $password = $_ENV['MYSQL_PASSWORD'] ?? '';
+                $host = getRequiredEnv('MYSQL_HOST');
+                $port = getOptionalEnvOrDefault('MYSQL_PORT', '3306');
+                $username = getRequiredEnv('MYSQL_USER');
+                $password = getRequiredEnv('MYSQL_PASSWORD');
 
                 $pdo = new PDO(
                     sprintf('mysql:host=%s;port=%d', $host, $port),
@@ -39,29 +65,25 @@ use Brick\Geo\Engine\GEOSEngine;
                 $statement = $pdo->query('SELECT VERSION()');
                 $version = $statement->fetchColumn();
 
-                echo 'MySQL version: ' . $version . PHP_EOL;
+                echo 'MySQL version: ', $version, PHP_EOL;
 
                 $engine = new PDOEngine($pdo);
                 break;
 
             case 'PDO_PGSQL':
-                echo 'Using PDOEngine for PostgreSQL' . PHP_EOL;
+                echo 'Using PDOEngine for PostgreSQL', PHP_EOL;
 
-                $credentials = [
-                    'host' => $_ENV['POSTGRES_HOST'] ?? 'localhost',
-                    'port' => $_ENV['POSTGRES_PORT'] ?? '5432',
-                    'username' => $_ENV['POSTGRES_USER'] ?? 'postgres',
-                    'password' => $_ENV['POSTGRES_PASSWORD'] ?? 'postgres',
-                ];
+                $host = getRequiredEnv('POSTGRES_HOST');
+                $port = getOptionalEnvOrDefault('POSTGRES_PORT', '5432');
+                $username = getRequiredEnv('POSTGRES_USER');
+                $password = getRequiredEnv('POSTGRES_PASSWORD');
 
                 $pdo = new PDO(
-                    vsprintf('pgsql:host=%s;port=%d', [
-                        $credentials['host'],
-                        $credentials['port'],
-                    ]),
-                    $credentials['username'],
-                    $credentials['password']
+                    sprintf('pgsql:host=%s;port=%d', $host, $port),
+                    $username,
+                    $password,
                 );
+
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
                 $pdo->exec('CREATE EXTENSION IF NOT EXISTS postgis;');
@@ -69,29 +91,29 @@ use Brick\Geo\Engine\GEOSEngine;
                 $statement = $pdo->query('SELECT version()');
                 $version = $statement->fetchColumn();
 
-                echo 'PostgreSQL version: ' . $version . PHP_EOL;
+                echo 'PostgreSQL version: ', $version, PHP_EOL;
 
                 $statement = $pdo->query('SELECT PostGIS_Version()');
                 $version = $statement->fetchColumn();
 
-                echo 'PostGIS version: ' . $version . PHP_EOL;
+                echo 'PostGIS version: ', $version, PHP_EOL;
 
                 $engine = new PDOEngine($pdo);
                 break;
 
             case 'SQLite3':
-                echo 'Using SQLite3Engine' . PHP_EOL;
+                echo 'Using SQLite3Engine', PHP_EOL;
 
                 $sqlite3 = new SQLite3(':memory:');
                 $sqlite3->enableExceptions(true);
 
                 $sqliteVersion = $sqlite3->querySingle('SELECT sqlite_version()');
-                echo 'SQLite version: ' . $sqliteVersion . PHP_EOL;
+                echo 'SQLite version: ', $sqliteVersion, PHP_EOL;
 
                 $sqlite3->loadExtension('mod_spatialite.so');
 
                 $spatialiteVersion = $sqlite3->querySingle('SELECT spatialite_version()');
-                echo 'SpatiaLite version: ' . $spatialiteVersion . PHP_EOL;
+                echo 'SpatiaLite version: ', $spatialiteVersion, PHP_EOL;
 
                 $sqlite3->exec('SELECT InitSpatialMetaData()');
 
@@ -99,14 +121,14 @@ use Brick\Geo\Engine\GEOSEngine;
                 break;
 
             case 'GEOS':
-                echo 'Using GEOSEngine' . PHP_EOL;
-                echo 'GEOS version: ' . GEOSVersion() . PHP_EOL;
+                echo 'Using GEOSEngine', PHP_EOL;
+                echo 'GEOS version: ', GEOSVersion(), PHP_EOL;
 
                 $engine = new GEOSEngine();
                 break;
 
             default:
-                echo 'Unknown engine: ' . $engine . PHP_EOL;
+                echo 'Unknown engine: ', $engine, PHP_EOL;
                 exit(1);
         }
 
